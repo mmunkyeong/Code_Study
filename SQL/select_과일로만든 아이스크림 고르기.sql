c73fc467a31d7ffd8a52e6ccbfb7ebcb4571297e
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-11-17 13:11:08 +0900

    Update select_과일로만든 아이스크림 고르기.sql

--- SQL/select_과일로만든 아이스크림 고르기.sql
+++ SQL/select_과일로만든 아이스크림 고르기.sql
@@ -1,11 +1,9 @@
 ##문제
 ##상반기 아이스크림 총주문량이 3,000보다 높으면서 아이스크림의 주 성분이 과일인 아이스크림의 맛을 총주문량이 큰 순서대로 조회하는 SQL 문을 작성해주세요.
 
-
 select FLAVOR
 from FIRST_HALF
 where TOTAL_ORDER>3000 and FLAVOR in(select FLAVOR
                                  from ICECREAM_INFO
                                  where INGREDIENT_TYPE="fruit_based")
-group by TOTAL_ORDER;
-
+order by TOTAL_ORDER desc;
