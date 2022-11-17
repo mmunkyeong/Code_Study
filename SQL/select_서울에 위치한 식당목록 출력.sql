baa92093307dfd23ddfce36379f9e1852d6aef55
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-11-17 15:45:30 +0900

    Update select_서울에 위치한 식당목록 출력.sql

--- SQL/select_서울에 위치한 식당목록 출력.sql
+++ SQL/select_서울에 위치한 식당목록 출력.sql
@@ -3,7 +3,6 @@
 #이때 리뷰 평균점수는 소수점 세 번째 자리에서 반올림 해주시고 결과는 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬해주세요.
 
 
-
 SELECT REST_INFO.REST_ID, REST_NAME, 
 FOOD_TYPE, FAVORITES, ADDRESS,
 ROUND(AVG(REVIEW_SCORE),2)AS SCORE
