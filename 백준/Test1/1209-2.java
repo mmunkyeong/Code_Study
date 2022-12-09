e32ddd492102a180723f58feed372a64d0a1a2c6
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-12-09 15:33:55 +0900

    Create 1209-2.java

---
+++ 백준/Test1/1209-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner sc= new Scanner(System.in);
+		double a=sc.nextInt();
+		double b=sc.nextInt();
+		System.out.print(a/b);
+		
+		sc.close();
+	}
+
+}
