dc0516adbe559dea7688ac56c6e8496370c450a9
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-12-11 22:14:00 +0900

    Create 1211-1.java

---
+++ 백준/Test1/1211-1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		long a=sc.nextInt();
+		long b=sc.nextInt();
+		System.out.println(Math.abs(a-b));
+		
+		sc.close();
+	}
+
+}
