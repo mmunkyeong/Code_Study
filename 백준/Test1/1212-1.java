d63b64d6ad6730cbe5d029016a6642fb12ad0efe
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-12-12 16:50:49 +0900

    Create 1212-1.java

---
+++ 백준/Test1/1212-1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		int e=sc.nextInt();
+		sc.close();
+		
+			System.out.print((a*a+b*b+c*c+d*d+e*e)%10);
+
+   }
+}
