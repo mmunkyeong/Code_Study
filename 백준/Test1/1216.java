7137730b169e35fd66484275e10c764ebe0bcd7d
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-12-16 17:08:20 +0900

    Create 1216.java

---
+++ 백준/Test1/1216.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		if(n%4==0) {
+			if(n%400==0) 
+			     System.out.println("1");
+		    else if (n%100==0) 
+			     System.out.println("0");
+	     	else 
+			     System.out.println("1");
+		}	
+		else System.out.println(0);
+		
+   }
+}
