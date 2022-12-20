e0bfd486678afbb20881e238eb850e5e7ae643c0
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2022-12-20 14:18:56 +0900

    Create 12209.java

---
+++ programmers/입문/java/1220/12209.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(float num1, float num2) {
+        int answer = 0;
+        answer=(int)((num1/num2)*1000);    // float를 int로 형 변환 
+        return answer;
+    }
+}
