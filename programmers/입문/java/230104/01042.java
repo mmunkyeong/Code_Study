fe24a74f8ca922e3297a8e5a2efc4bfaf55aa314
문 경 <62290451+mmunkyeong@users.noreply.github.com>
2023-01-04 13:55:14 +0900

    Create 01042.java

---
+++ programmers/입문/java/230104/01042.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        for(int i=0; i<(age+"").length(); i++){
+            answer+=(char)((age+"").charAt(i)+49);
+        }
+        return answer;
+    }
+}
